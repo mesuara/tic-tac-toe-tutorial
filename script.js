3233dd57066a3c54d3220be9f1967fae749efe9f
mesuara <37057206+mesuara@users.noreply.github.com>
2023-09-27 19:44:30 +0000

    adding js funcitons

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 /*----- constants -----*/
+
+
 /*----- app's state (variables) -----*/
+let board;
+let turn = 'X';
+
+function init() {
+board = [
+'', '', '',
+'', '', '',
+'', '', ''
+];
+
+  render();
+};
+
+init();
+
+function render() {
+board.forEach(function(mark, index){
+
+  squares[index].textContent = mark;
+
+console.log(mark, index);
+});
+};
+
 /*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+
 /*----- event listeners -----*/
+
+
 /*----- functions -----*/
+function handleTurn(event) {
+let idx = squares.findIndex(function(square) {
+return square === event.target;
+});
+};
+
